cb9e0246aaef768786f5b81898ccd3474cb0d414
Vyom Kumar <62970161+chimeconfused999@users.noreply.github.com>
2024-06-04 10:21:13 -0700

    Create q.js

---
+++ Playground/q.js
@@ -0,0 +1,27 @@
+var q = 
+[
+  {
+    "Description": "Q1",
+    "Choice": [
+      {"Description": "A"},
+      {"Description": "B" },
+      {"Description": "C"}
+      ]
+  },
+  {
+    "Description": "Q2",
+    "Choice": [
+      {"Description": "A"},
+      {"Description": "B" },
+      {"Description": "C"}
+      ]
+  },
+  {
+    "Description": "Q3",
+    "Choice": [
+      {"Description": "A"},
+      {"Description": "B" },
+      {"Description": "C"}
+      ]
+  }
+]
